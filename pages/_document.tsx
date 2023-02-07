7e4ea351f02778ea5f8df0b132640f16a07ea25f
Shogo Kitada <contact@shogo0x2e.com>
2023-02-07 15:52:15 +0900

    favicon を追加

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        {/* 
+          favicon 生成に使ったサイト: 
+          https://redketchup.io/favicon-generator 
+        */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </Head>
+      
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
+/*
+    File Name   : index.tsx
+    Created     : on 18:25 at Feb 06, 2023
+    Description : ポートフォリサイトの index
+
+    Copyright 2023 Shogo Kitada All Rights Reserved.
+        contact@shogo0x2e.com (Twitter, GitHub: @shogo0x2e)
+
+    I would be happy to notify me if you use part of my code.
+*/
 import Head from "next/head"
 import Link from "next/link"
-import styles from "@/styles/Home.module.css"
+import styles from "@/styles/Home.module.scss"
+import Naviagtion from "@/components/Navigation"
 
 export default function Home() {
   return (
@@ -9,17 +20,9 @@ export default function Home() {
         <title>Shogo Kitada</title>
         <meta name="description" content="This site provides detailed information about Shogo Kitada (shogo0x2e)." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-        {/* 
-          favicon 生成に使ったサイト: 
-          https://redketchup.io/favicon-generator 
-        */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className={styles.main}>
+        <Naviagtion />
         <Link href="/works">
           作品たちはこちら
         </Link>
